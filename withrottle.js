1f3f39be75a0e4be330e656d8d2be6e332101d0a
railnerd <railnerd@me.com>
2013-03-10 22:21:47 -0700

    track per throttle properties

--- withrottle.js
+++ withrottle.js
@@ -18,7 +18,8 @@ var	WiThrottle = function(name, port, cmdStation, callback) {
 	self.server = net.createServer( function (s) {
 		// keep track of new connection in clients object
 		s.__clientKey = s.remoteAddress;
-		self.clients[s.__clientKey]={'address': s.remoteAddress};
+		self.clients[s.__clientKey]={'ip': s.remoteAddress};
+		console.log("connect: "+util.inspect(self.clients));
 
 		self.lineHandler = carrier.carry(s);
 
@@ -28,6 +29,7 @@ var	WiThrottle = function(name, port, cmdStation, callback) {
 		s.on('end', function() {
 			// forget this client
 			delete self.clients[s.__clientKey];
+			console.log("DISCONNECT: "+util.inspect(self.clients));
 		});
 		
 		s.on('error', function (e) {
@@ -70,41 +72,70 @@ var	WiThrottle = function(name, port, cmdStation, callback) {
 util.inherits(WiThrottle, EventEmitter);
 
 
+WiThrottle.prototype.getThrottleProperty = function (s,whichThrottle,key) {
+	if (this.clients[s.__clientKey][whichThrottle] === undefined) {
+		return undefined;
+	} else {
+		return this.clients[s.__clientKey][whichThrottle][key];
+	}
+}
+
+
+WiThrottle.prototype.setThrottleProperty = function (s,whichThrottle,prop,val) {
+	if (this.clients[s.__clientKey][whichThrottle] === undefined) {
+		this.clients[s.__clientKey][whichThrottle] = {};
+	}
+	this.clients[s.__clientKey][whichThrottle][prop] = val;
+}
+
 WiThrottle.prototype.doThrottleCommand = function (s, whichThrottle, msg) {
-	var cmd = msg.charAt(0),
+	var self = this,
+		cmd = msg.charAt(0),
 		arg = msg.slice(1);
 
 	switch (cmd) {
 		case 'L':
 		case 'S':
-			console.log(whichThrottle + " ADDRESS: " + arg + " ("+cmd+")");
+			self.setThrottleProperty(s,whichThrottle,'address',arg);
+			console.log("SET ADDRESS: " + util.inspect(self.clients));
 			s.write(whichThrottle + arg);
 			break;
 
 		case 'R':
 			if (msg.charAt(1) == '0') {
-				console.log(whichThrottle + " REVERSE");
+				self.setThrottleProperty(s,whichThrottle,'direction','R');
+				console.log("REVERSE: " + util.inspect(self.clients[s.__clientKey]));
 			} else {
-				console.log(whichThrottle + " FORWARD");
+				self.setThrottleProperty(s,whichThrottle,'direction','F');
+				console.log("FORWARD: " + util.inspect(self.clients[s.__clientKey]));
 			}
 			break;
 
 		case 'I':
-			console.log(whichThrottle + " IDLE");
+			self.setThrottleProperty(s,whichThrottle,'speed',0);
+			console.log("IDLE: " + util.inspect(self.clients[s.__clientKey]));
 			break;
 
 		case 'X':
-			console.log(whichThrottle + " ESTOP");
+			self.setThrottleProperty(s,whichThrottle,'speed',1);
+			console.log("ESTOP: " + util.inspect(self.clients[s.__clientKey]));
 			break;
 
 		case 'V':
-			console.log(whichThrottle + " SPEED: " + arg);
+			if (self.getThrottleProperty(s,whichThrottle,'direction') === undefined) {
+				console.log("inferring forward direction");
+				self.setThrottleProperty(s,whichThrottle,'direction','F');
+			}
+			self.setThrottleProperty(s,whichThrottle,'speed',arg);
+			console.log("SPEED: " + util.inspect(self.clients[s.__clientKey]));
 			break;
 
 		case 'r':
 		case 'd':
 			console.log(whichThrottle + " RELEASE/DISPATCH");
-			s.write(whichThrottle+"Not Set");
+			delete self.clients[s.__clientKey][whichThrottle];
+			console.log("RELEASE: " + util.inspect(self.clients));
+			s.write(whichThrottle + "Not Set");
 			break;
 
 		default:
